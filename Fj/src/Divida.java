f87164e6784a4b1e3cb22435a477513731941a9c
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-22 23:06:35 -0300

    feat: criar classe dividas e seus atributos

---
+++ Fj/src/Divida.java
@@ -0,0 +1,41 @@
+package Fj.src;
+
+public class Divida {
+    private double total;
+    private double valorPago;
+    private String credor;
+    private String cnpjCredor;
+
+    public double getTotal() {
+        return this.total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public double getValorPago() {
+        return this.valorPago;
+    }
+
+    public void setValorPago(double valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public String getCredor() {
+        return this.credor;
+    }
+
+    public void setCredor(String credor) {
+        this.credor = credor;
+    }
+
+    public String getCnpjCredor() {
+        return this.cnpjCredor;
+    }
+
+    public void setCnpjCredor(String cnpjCredor) {
+        this.cnpjCredor = cnpjCredor;
+    }
+
+}
