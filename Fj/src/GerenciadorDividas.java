e2ebbe933efb3a69a8b7726addb5d892c9e9500d
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-22 23:49:26 -0300

    feat: instanciando pagamento no metodo efetuarpagamento e registrando

--- Fj/src/GerenciadorDividas.java
+++ Fj/src/GerenciadorDividas.java
@@ -1,6 +1,10 @@
 package Fj.src;
 public class GerenciadorDividas {
-    public void efetuarPagamento(Divida divida, double valor){
-     divida.paga(valor);
+    public void efetuarPagamento(Divida divida, String nomePagador, String cnpjPagador, double valor){
+     Pagamento pagamento = new Pagamento();
+     pagamento.setCnpjPagador(cnpjPagador);
+     pagamento.setPagador(nomePagador);
+     pagamento.setValor(valor);   
+     divida.registra(pagamento);
     }
 }
