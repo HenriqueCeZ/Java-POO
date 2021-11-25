5c4758c815bb17798d0db41732248471acd59402
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-25 00:12:49 -0300

    feat: strategy

--- fj11/src/TestaConta.java
+++ fj11/src/TestaConta.java
@@ -2,7 +2,12 @@ public class TestaConta {
     public static void main(String[] args) {
      Conta conta = new Conta(400.5,"Henrique",1234);
 
-     System.out.println(conta.getNome());
-     conta.imprime();
+     int [] nums = new int[5];
+
+     for(int i = 0; i < nums.length; i++){
+         nums[i] = i * 2;
+         System.out.println(nums[i]);
+     }
+     
     }
 }
