4f17537ff5fd74a0c8755990d95ba625365ac91a
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-26 03:08:13 -0300

    feat: implement class SemDesconto

---
+++ design-patterns/strategy/src/SemDesconto.java
@@ -0,0 +1,14 @@
+public class SemDesconto implements Desconta{
+    
+   @Override
+   public double desconta(Orcamento orcamento){
+       return 0;
+   }
+
+   @Override
+   public void setProximo(Desconta proximo){
+      
+   }
+
+   
+}
