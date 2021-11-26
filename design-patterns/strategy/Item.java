d79ddd205bb53c68316b52b1a87f6b201bc12cba
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-26 02:25:52 -0300

    feat: implement class item

---
+++ design-patterns/strategy/Item.java
@@ -0,0 +1,21 @@
+public class Item {
+    private final String nome;
+    private final double valor;
+    public  Item(String nome, double valor){
+        this.nome = nome;
+        this.valor = valor;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+
+
+    public double getValor() {
+        return this.valor;
+    }
+    
+}
+
+  
+
+ 
