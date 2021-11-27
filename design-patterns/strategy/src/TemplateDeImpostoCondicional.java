b367b77e7a591ad88460710ec9642e59877fa677
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-26 21:12:11 -0300

    feat: create class TemplateDeImpostoCondicional and methods

---
+++ design-patterns/strategy/src/TemplateDeImpostoCondicional.java
@@ -0,0 +1,16 @@
+public abstract class TemplateDeImpostoCondicional implements Imposto {
+
+    @Override
+        public double calcula(Orcamento orcamento){
+            if(deveUsarMaximaTaxacao(orcamento)){
+                return maximaTaxacao(orcamento);
+            }else{
+                return maximaTaxacao(orcamento);
+            }
+        }
+
+   public abstract double maximaTaxacao(Orcamento orcamento);
+   public abstract double minimaTaxacao(Orcamento orcamento);
+   public abstract boolean deveUsarMaximaTaxacao(Orcamento orcamento);
+    
+}
